b26357353388ae22c8879cf07f36f4e2e5fbd529
viniciuss1812 <115712261+viniciuss1812@users.noreply.github.com>
2025-09-15 21:06:16 -0300

    ultimo

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
 const buttons = document.querySelectorAll('.button');
-
 const videoFrame = document.getElementById('video-frame');
-
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    const videoUrl = button.getAttribute('data-video');
+    let videoUrl = button.getAttribute('data-video');
+
+    // Se for link do YouTube, transforma em embed
+    if (videoUrl.includes("watch?v=")) {
+      videoUrl = videoUrl.replace("watch?v=", "embed/");
+    } else if (videoUrl.includes("youtu.be/")) {
+      videoUrl = videoUrl.replace("youtu.be/", "www.youtube.com/embed/");
+    }
+
     videoFrame.src = videoUrl;
   });
 });
+
